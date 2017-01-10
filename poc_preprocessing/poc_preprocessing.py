5b88f922d4f0ad753ab175a1c67884d7797a6f8e
eclipse95 <alexis.bitaillou@etu.univ-nantes.fr>
2017-01-10 17:15:55 +0100

    correct output CSV file

--- poc_preprocessing/poc_preprocessing.py
+++ poc_preprocessing/poc_preprocessing.py
@@ -16,14 +16,19 @@ def reading(filename: str) -> list:
 
 def process(data: list) -> list:
     new_data = list()
-    for i in range(1, len(data)):
-        new_data.append([data[i - 1][0], int(data[i - 1][2]) - int(data[i - 1][1])])
-        new_data.append([data[i - 1][0] + "-" + data[i][0], int(data[i][1]) - int(data[i - 1][2])])
+    for i in range(len(data)):
+        new_data.append([data[i][0], int(data[i][2]) - int(data[i][1])])
     return new_data
 
 
+def process_interkey(data:list)->list:
+    new_data = list()
+    for i in range(1, len(data)):
+        new_data.append([data[i - 1][0] + "-" + data[i][0], int(data[i][1]) - int(data[i - 1][2])])
+    return new_data
+
 def writing(filename: str, data: list):
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8', newline='') as file:
         csv_writer = csv.writer(file, delimiter=';')
         for i in range(len(data)):
             csv_writer.writerow(data[i])
