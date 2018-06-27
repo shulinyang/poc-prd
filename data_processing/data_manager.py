acd3aa8342332d0fd84beb694abadf9434a5934a
eclipse95 <9960386+eclipse95@users.noreply.github.com>
2018-06-27 23:27:32 +0200

    data processing: data manager is ready

--- data_processing/data_manager.py
+++ data_processing/data_manager.py
@@ -8,12 +8,13 @@ from typing import List, NoReturn, Tuple
 
 class DataManager():
     def __init__(self):
+        self.dataset1 = None  # type: List
+        self.dataset2 = None  # type: List
         self.mapper = dict()  # type: dict
 
     def mapper_scan(self, dataset: List[List]) -> NoReturn:
         for i in dataset:
-            for j in i:
-                self._add_vkcode(int(j[0]))
+            self._add_vkcode(int(i[0]))
 
     def _add_vkcode(self, vk: int) -> NoReturn:
         if vk not in self.mapper.keys():
@@ -27,15 +28,19 @@ class DataManager():
             self.mapper[key] = [0] * counter + [1] + [0] * (len(self.mapper.keys()) - counter) + [0]
             counter += 1
 
-    def shuffle_data(self, dataset: List) -> List:
-        data = dataset.copy()
-        for i in range(int(len(data) / 2)):
-            swap_idx = int(random.randint(0, 65556) % len(data))
+    def shuffle_data(self) -> NoReturn:
+        for i in range(int(len(self.dataset1) / 2)):
+            swap_idx = int(random.randint(0, 65556) % len(self.dataset1))
             if i != swap_idx:
-                tmp = data[swap_idx]
-                data[swap_idx] = data[i]
-                data[i] = tmp
-        return data
+                tmp = self.dataset1[swap_idx]
+                self.dataset1[swap_idx] = self.dataset1[i]
+                self.dataset1[i] = tmp
+        for i in range(int(len(self.dataset2) / 2)):
+            swap_idx = int(random.randint(0, 65556) % len(self.dataset2))
+            if i != swap_idx:
+                tmp = self.dataset2[swap_idx]
+                self.dataset2[swap_idx] = self.dataset2[i]
+                self.dataset2[i] = tmp
 
     def export_csv(self, filename: str, dataset: List) -> None:
         with open(filename + ".csv", "w", encoding='utf8', newline='') as csv_file:
@@ -47,53 +52,60 @@ class DataManager():
                 csv_writer.writerow(key)
         print(filename + " as been exported.")
 
-    def process_key(self, dataset1: List, dataset2: List, training_percent: float = 0.80) -> Tuple[
-        List[List], List[List]]:
-        def _inner_process(self: DataManager, dataset: List[List], user_id: int) -> List[List]:
-            data = list()
-            for i in range(len(dataset)):
-                if 3000 > (int(dataset[i][2]) - int(dataset[i][1])) > 16:
-                    data.append(
-                        self.mapper[dataset[i][0]] + [(float(dataset[i][2]) - float(dataset[i][1])) / 3000] + [user_id])
+    def process_key(self, training_percent: float = 0.80) -> Tuple[List[List], List[List]]:
+        data1 = self.__inner_process(self.dataset1, 0)  # type: List[List]
+        data2 = self.__inner_process(self.dataset2, 1)  # type: List[List]
+        final_length = int(min(len(data1), len(data2)) * training_percent)  # type: int
+        return data1[:final_length] + data2[:final_length], data1[final_length:] + data2[final_length:]
+
+    def __inner_process(self, dataset: List[List], user_id: int) -> List[List]:
+        data = list()
+        for i in range(len(dataset)):
+            if 3000 > (int(dataset[i][2]) - int(dataset[i][1])) > 16:
+                data.append(
+                    self.mapper[int(dataset[i][0])] + [(float(dataset[i][2]) - float(dataset[i][1])) / 3000] + [
+                        user_id])
+        return data
+
+    def process_inter(self, training_percent: float = 0.80) -> Tuple[List, List]:
+        data1 = self.__inner_process_interkey(self.dataset1, 0)  # type: List[List]
+        data2 = self.__inner_process_interkey(self.dataset2, 1)  # type: List[List]
+        final_length = int(min(len(data1), len(data2)) * training_percent)  # type: int
+        train_dataset = data1[:final_length] + data2[:final_length]
+        test_dataset = data1[final_length:] + data2[final_length:]
+        return train_dataset, test_dataset
+
+    def __inner_process_interkey(self, dataset: List[List], user_id: int) -> List[List]:
+        def formating_interkey(vector1: List[int], vector2: List[int]):
+            data = [x1 | x2 for x1, x2 in zip(vector1, vector2)]
+            data[len(data) - 1] = 1
             return data
 
-        data1 = _inner_process(self, dataset1, 0)  # type: List[List]
-        data2 = _inner_process(self, dataset2, 1)  # type: List[List]
-        final_length = int(min(len(data1), len(data2)) * training_percent)  # type: int
-        train_dataset = data1[:final_length] + data2[:final_length]
-        test_dataset = data1[final_length:] + data2[final_length:]
-        return train_dataset, test_dataset
+        new_data = list()
+        composite_key = [160, 161, 162, 163]
+        for i in range(1, len(dataset)):
+            duration = int(dataset[i][1]) - int(dataset[i - 1][2])
+            last_vkcode = int(dataset[i][0])
+            if 10 < duration < 8000 and last_vkcode not in composite_key:
+                new_data.append(
+                    formating_interkey(self.mapper[int(dataset[i - 1][0])], self.mapper[int(dataset[i][0])]) + [
+                        duration / 8000,
+                        user_id])
+        return new_data
 
-    def process_inter(self, dataset1: List, dataset2, training_percent: float = 0.80) -> Tuple[List, List]:
-        def _inner_process_interkey(self: DataManager, dataset: List[List], user_id: int) -> List[List]:
-            def formating_interkey(vector1: List[int], vector2: List[int]):
-                data = [x1 | x2 for x1, x2 in (vector1, vector2)]
-                data[len(data) - 1] = 1
-                return data
-
-            new_data = list()
-            composite_key = [160, 161, 162, 163]
-            for i in range(1, len(dataset)):
-                duration = int(dataset[i][1]) - int(dataset[i - 1][2])
-                last_vkcode = int(dataset[i][0])
-                if 10 < duration < 8000 and last_vkcode not in composite_key:
-                    new_data.append(
-                        formating_interkey(self.mapper[dataset[i - 1][0]], self.mapper[dataset[i][0]]) + [duration,
-                                                                                                          user_id])
-            return new_data
-
-        data1 = _inner_process_interkey(self, dataset1, 0)  # type: List[List]
-        data2 = _inner_process_interkey(self, dataset2, 1)  # type: List[List]
-        final_length = int(min(len(data1), len(data2)) * training_percent)  # type: int
-        train_dataset = data1[:final_length] + data2[:final_length]
-        test_dataset = data1[final_length:] + data2[final_length:]
-        return train_dataset, test_dataset
+    def load_dataset(self, dataset1: List, dataset2: List):
+        self.dataset1 = dataset1
+        self.dataset2 = dataset2
 
     def batch(self, dataset1: List, dataset2: List, output_filename):
+        self.load_dataset(dataset1, dataset2)
         self.mapper_scan(dataset1)
         self.mapper_scan(dataset2)
         self._generate_vector()
-        train_inter, test_inter = self.process_inter(dataset1, dataset2)
-        train, test = self.process_key(self.shuffle_data(dataset1), self.shuffle_data(dataset2))
+        train_inter, test_inter = self.process_inter()
+        self.shuffle_data()
+        train, test = self.process_key()
+        # self.export_csv(output_filename + "-train", train)
+        # self.export_csv(output_filename + "-test", test)
         self.export_csv(output_filename + "-train", train + train_inter)
-        self.export_csv(output_filename + "test", test + test_inter)
+        self.export_csv(output_filename + "-test", test + test_inter)
--- data_processing/processing_kbd_data.py
+++ data_processing/processing_kbd_data.py
@@ -11,9 +11,7 @@ import sys
 from typing import List
 
 from common_functions import reading
-from dataSorting import DataSorting
 from data_manager import DataManager
-from mapper import Mapper
 from table_vkcode import x11_code_to_vkcode
 
 
@@ -53,59 +51,6 @@ def preprocess(data: List[List]) -> List[List]:
     return new_data
 
 
-def process(data: List[List], user_id: int, threshold: int = 3000) -> List[List]:
-    """Compute duration between pressed timestamp and release timestamp
-    <!> hard code
-    :param data: list like [[vkcode, timestamp1, timestamp2], [vkc, t1, t2]]
-    :param threshold: timeout
-    :return: list like [[vkcode, duration][vkcode, duration]]
-    """
-    new_data = list()
-    for i in range(len(data)):
-        if threshold > (int(data[i][2]) - int(data[i][1])) > 16:
-            new_data.append(
-                categorical_effect(data[i][0]) + [(float(data[i][2]) - float(data[i][1])) / threshold] + [user_id])
-    return new_data
-
-
-def categorical_effect(number: str) -> List[int]:
-    return Mapper.vkcode[number]
-
-
-def process_interkey(data: List[List], threshold: int = 8000) -> List[List]:
-    """Compute duration between two keys
-    <!> hard code
-    :param data: list like [[vkcode, timestamp1, timestamp2], [vkc, t1, t2]]
-    :param threshold: timeout between two keys in ms (default: 2000)
-    :return: list like [[vkcode0vkcode, duration],[vkcode0vkcode, duration]]
-    """
-
-    def format_interkey_name(string: str) -> str:
-        if int(string) < 100:
-            return "0" + string
-        return string
-
-    new_data = list()
-    composite_key = [160, 161, 162, 163]
-    for i in range(1, len(data)):
-        duration = int(data[i][1]) - int(data[i - 1][2])
-        last_vkcode = int(data[i][0])
-        if 1 < duration < threshold and last_vkcode not in composite_key:
-            new_data.append([format_interkey_name(data[i - 1][0]) + format_interkey_name(data[i][0]), duration])
-    return new_data
-
-
-def reformatting_inter_keys(local_data: List[List], user_id: int):
-    for i in range(len(local_data)):
-        vkcode = str(local_data[i].pop(0))
-        revered_list = reversed(Mapper.vkcode[vkcode])
-        for elem in revered_list:
-            local_data[i].insert(0, elem)
-        local_data[i].append(user_id)
-
-    return local_data
-
-
 def prepare_x11(basename) -> None:
     """Function to process data from X11 agent
     <!> hard code
@@ -119,41 +64,8 @@ def prepare_x11(basename) -> None:
             csv_writer.writerow(local_data[i])
 
 
-def meta(list_basename: list, user_id: List[int]) -> None:
-    """Function to process data (all agents)
-    <!> hard code
-    :param list_basename: list like [basename1, basename2]
-    :return: None
-    """
-    meta_prepare_mapper(list_basename, False)
-    Mapper.generate_vector()
-    for i in range(len(list_basename)):
-        local_data = reading(list_basename[i] + ".data")
-        DataSorting(process(local_data, user_id[i])).export_csv(list_basename[i])
-
-
-def meta_inter_key(list_basename: list, user_id: List[int]) -> None:
-    meta_prepare_mapper(list_basename, True)
-    Mapper.generate_vector()
-    for i in range(len(list_basename)):
-        local_data = reading(list_basename[i] + ".data")
-        DataSorting(reformatting_inter_keys(process_interkey(local_data), user_id[i])).export_csv(
-            list_basename[i] + "-inter")
-
-
-def meta_prepare_mapper(list_basename: list, inter_key: bool):
-    for i in range(len(list_basename)):
-        local_data = reading(list_basename[i] + ".data")
-        if not inter_key:
-            for j in range(len(local_data)):
-                Mapper.add_vkcode(local_data[j][0])
-        else:
-            for elem in process_interkey(local_data):
-                Mapper.add_vkcode(elem[0])
-
-
 if __name__ == '__main__':
-    prepare_x11("remi")
+    # prepare_x11("remi")
     dm = DataManager()
     dm.batch(reading("alexis.data"), reading("remi.data"), "alexis-remi")
     # meta(["alexis", "nicolas", "remi"] [0, 1, 2])  # <!> hard coded
